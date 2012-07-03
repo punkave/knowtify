8fb6641e08235d89cd90654da92152386cc2eae3
Gabriel Farrell <g@grrawr.com>
2012-07-03 17:39:36 -0400

    Simplify routes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var fs = require('fs')
 var http = require('http')
 var routes = require('./routes')
 var whiskers = require('whiskers')
@@ -5,9 +6,12 @@ var whiskers = require('whiskers')
 var port = process.env.PORT || 8080
 
 http.createServer(function(req, res) {
-  res.render = function(template, context) {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.end(whiskers.render(template, context))
+  res.render = function(filename, context) {
+    fs.readFile('templates/'+filename, function(err, template) {
+      if (err) throw err
+      res.writeHead(200, {'Content-Type': 'text/html'})
+      res.end(whiskers.render(template, context))
+    })
   }
   routes(req, res)
 }).listen(port, function() {
