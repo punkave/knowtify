a1473eb771ba05f72ef51f84d1b6d6edfea4a284
Gabriel Farrell <g@grrawr.com>
2012-07-05 11:04:25 -0400

    Login page with body parsing

--- routes.js
+++ routes.js
@@ -1,7 +1,26 @@
+var querystring = require('querystring')
 var router = require('choreographer').router()
 
 router.get('/', function(req, res) {
   res.render('index.html', {title: 'Status'})
+})
+
+router.get('/login', function(req, res) {
+  res.render('login.html')
+})
+
+router.post('/login', function(req, res) {
+  var data = '';
+  req.setEncoding('utf8')
+  req.on('data', function(chunk) {
+    data += chunk
+  })
+  req.on('end', function() {
+    var body = querystring.parse(data)
+    console.log(body)
+  })
+  res.writeHead(303, {Location: '/login'})
+  res.end()
 })
 
 module.exports = function() {
