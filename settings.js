44aae4ca1d90cfd7144d4303cad2da5587c1151a
Gabriel Farrell <g@grrawr.com>
2012-07-18 09:41:34 -0400

    Added secret to settings

--- settings.js
+++ settings.js
@@ -1,6 +1,13 @@
 var db = require('./db')
+var util = require('util')
 
-// TODO move settings to DB
+// TODO move some settings to DB
+if (process.env.SECRET) {
+  exports.secret = process.env.SECRET
+} else {
+  util.log('No secret key provided for API. Using default instead.')
+  exports.secret = 'SEKRIT'
+}
 exports.monitor = process.env.MONITOR || 'http://127.0.0.1:5001'
 exports.title = process.env.TITLE || 'Status'
 exports.infobox = process.env.INFOBOX || 'These are some things we want to say. If you have questions or comments, you can contact us at <a href="mailto:info@example.com">info@example.com</a>.'
